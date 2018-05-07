4336332d0744f4c64b6fa13e7f90cf333958c631
Pedro Quintero <pecuerre@gmail.com>
2018-05-07 16:21:19 -0400

    moving functionalities

--- app/resources/page_content_resource.rb
+++ app/resources/page_content_resource.rb
@@ -3,8 +3,6 @@ class PageContentResource < JSONAPI::Resource
 
     attributes :url, :content
 
-    before_save :fetch_content
-
     def self.updateble_fields(context)
         super - [:content]
     end
@@ -13,23 +11,5 @@ class PageContentResource < JSONAPI::Resource
         super - [:content]
     end
 
-    def fetch_content
-        original_content = PageContent.get(self.url)
-        new_content = ""
-        doc = Nokogiri::HTML(original_content)
-        doc.css('h1').each do |link|
-            new_content += "# " + link.content + '\n'
-        end
-        doc.css('h2').each do |link|
-            new_content += "## " + link.content + '\n'
-        end
-        doc.css('h3').each do |link|
-            new_content += "### " + link.content + '\n'
-        end
-        doc.css('a').each do |link|
-            new_content += "[" + link['href'] + "]\n"
-        end
-        self.content = new_content
-    end
-end
+ end
 
