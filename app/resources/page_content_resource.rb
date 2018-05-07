264f3cfaf38487016623cbbea81e086ff30a8f7d
Pedro Quintero <pecuerre@gmail.com>
2018-05-07 14:45:03 -0400

    updatable and creatable attributes

--- app/resources/page_content_resource.rb
+++ app/resources/page_content_resource.rb
@@ -2,4 +2,12 @@ class PageContentResource < JSONAPI::Resource
     immutable
 
     attributes :url, :content
+
+    def self.updateble_fields
+        super - [:content]
+    end
+
+    def self.creatable_fields
+        super - [:content]
+    end
 end
